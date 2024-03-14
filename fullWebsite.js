6a4c0a5649b0b4cde19a9860a07a86bcd56c2e4e
Huy-Du-jh <huy.du@journeyh.io>
2024-03-14 12:22:08 +0700

    merged all addEventListeners into one function, finished the 2nd carousel and the paragraph section below it

--- fullWebsite.js
+++ fullWebsite.js
@@ -1,4 +1,16 @@
-function dragCarousel() {
+
+function swipeContainerLeft(el) {
+  el.style.transition = "none";
+  el.style.transform = "translateX(0)";
+  el.style.left = (Number(el.style.left.replace('px', '')) - 100) + "px";
+}
+function swipeContainerRight(el) {
+  el.style.transition = "none";
+  el.style.transform = "translateX(0)";
+  el.style.left = (Number(el.style.left.replace('px', '')) + 100) + "px";
+}
+
+function addEventListenersOnLoad() {
   const containerArr = document.querySelectorAll(".carouselContainer");
   containerArr.forEach(container => {
     const innerContainer = container.children[1];
@@ -24,15 +36,15 @@ function dragCarousel() {
       let outer = container.getBoundingClientRect();
       let inner = innerContainer.getBoundingClientRect();
 
-      if (parseInt(innerContainer.style.left) > 250) {
-        // innerContainer.style.left = "0px";
-        innerContainer.style.left = "250px";
-      }
+      // if (parseInt(innerContainer.style.left) > 250) {
+      //   // innerContainer.style.left = "0px";
+      //   innerContainer.style.left = "250px";
+      // }
 
-      if (inner.right < outer.right - 250) {
-        // innerContainer.style.left = `-${inner.width - outer.width}px`;
-        innerContainer.style.left = inner.width - outer.width - 550 + "px";
-      }
+      // if (inner.right < outer.right - 250) {
+      //   // innerContainer.style.left = `-${inner.width - outer.width}px`;
+      //   innerContainer.style.left = inner.width - outer.width - 550 + "px";
+      // }
     };
     container.addEventListener("mousemove", (e) => {
       if (!pressed) return;
@@ -43,28 +55,7 @@ function dragCarousel() {
       boundItems();
     });
   });
-}
-function swipeContainerLeft(e) {
-  e.currentTarget.style.transition = "none";
-  e.currentTarget.style.transform = "translateX(100px)";
-  e.currentTarget.style.left = (Number(e.currentTarget.style.left.replace('px', '')) - 100) + "px";
-}
-function swipeContainerRight(e) {
-  e.currentTarget.style.transition = "none";
-  e.currentTarget.style.transform = "translateX(0)";
-  e.currentTarget.style.left = (Number(e.currentTarget.style.left.replace('px', '')) + 100) + "px";
-}
-function swipeContainerLeftElement(el) {
-  el.style.transition = "none";
-  el.style.transform = "translateX(0)";
-  el.style.left = (Number(el.style.left.replace('px', '')) - 100) + "px";
-}
-function swipeContainerRightElement(el) {
-  el.style.transition = "none";
-  el.style.transform = "translateX(0)";
-  el.style.left = (Number(el.style.left.replace('px', '')) + 100) + "px";
-}
-function navigateCarousel() {
+
   const leftArrowArr = document.querySelectorAll(".leftNavigate");
   leftArrowArr.forEach(leftArrow => {
     const rightArrow = leftArrow.parentElement.getElementsByClassName("rightNavigate")[0];
@@ -75,31 +66,29 @@ function navigateCarousel() {
       const container = innerContainer.parentElement;
       const outer = container.getBoundingClientRect();
       const inner = innerContainer.getBoundingClientRect();
-      if (inner.right <= outer.right - 250) {
-        // innerContainer.style.left = `-${inner.width - outer.width}px`;
-        innerContainer.style.left = inner.width - outer.width - 550 + "px";
-        return;
-      }
+      // if (inner.right <= outer.right - 250) {
+      //   // innerContainer.style.left = `-${inner.width - outer.width}px`;
+      //   innerContainer.style.left = inner.width - outer.width - 550 + "px";
+      //   return;
+      // }
       innerContainer.style.transition = "transform 0.5s ease-in-out";
-      innerContainer.style.transform = "translateX(-100px)";
-      const timeOutID = setTimeout(function(){swipeContainerLeftElement(innerContainer)},500);
+      innerContainer.style.transform = "translateX(100px)";
+      const timeOutID = setTimeout(function () { swipeContainerRight(innerContainer) }, 500);
     });
 
     // rightArrow.parentElement.children[0].children[1].addEventListener("transitionend", swipeContainerRight);
     rightArrow.addEventListener("click", (e) => {
       e.stopPropagation();
       const innerContainer = e.currentTarget.parentElement.children[0].children[1];
-      if (parseInt(innerContainer.style.left) >= 250) {
-        innerContainer.style.left = "250px";
-          return;
-      }
-    
+      // if (parseInt(innerContainer.style.left) >= 250) {
+      //   innerContainer.style.left = "250px";
+      //     return;
+      // }
+
       innerContainer.style.transition = "transform 0.5s ease-in-out";
-      innerContainer.style.transform = "translateX(100px)";
-      const timeOutID = setTimeout(function(){swipeContainerRightElement(innerContainer)},500);
+      innerContainer.style.transform = "translateX(-100px)";
+      const timeOutID = setTimeout(function () { swipeContainerLeft(innerContainer) }, 500);
     });
   });
-
 }
-dragCarousel();
-navigateCarousel();
+addEventListenersOnLoad();
